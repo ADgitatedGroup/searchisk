fa8d8769da33d3c6980cd8c2e2fe9c2554dcbaf8
Alexei Matyushkin <am@mudasobwa.ru>
2013-04-26 10:00:39 +0400

    Suggestions are now intelligent, basing on the input language.

--- background.js
+++ background.js
@@ -2,17 +2,67 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+function matchText(text) {
+  return "<match>" + text + "</match>";
+}
+
+function russianSuggest(text) {
+  text_p = text.replace(/\s+/g, "+");
+  return [
+    {
+      content: "http://slovari.yandex.ru/" + text + "/перевод/#lingvo", 
+      description: chrome.i18n.getMessage("translate") + " " + matchText(text) + " " + chrome.i18n.getMessage("against") + " " + chrome.i18n.getMessage("lingvo")
+    },
+    {
+      content: "http://yandex.ru/yandsearch?text=" + text, 
+      description: chrome.i18n.getMessage("search") + " " + matchText(text) + " " + chrome.i18n.getMessage("against") + " " + chrome.i18n.getMessage("yandex")
+    },
+    { 
+      content: "http://images.yandex.ru/yandsearch?text=" + text, 
+      description: chrome.i18n.getMessage("search") + " " + matchText(text) + " " + chrome.i18n.getMessage("against") + " " + chrome.i18n.getMessage("yandex_images")
+    },
+    {
+      content: "http://ru.wikipedia.org/wiki/" + text, 
+      description: chrome.i18n.getMessage("search") + " " + matchText(text) + " " + chrome.i18n.getMessage("against") + " " + chrome.i18n.getMessage("russian_wikipedia")
+    },
+    {
+      content: "http://www.youtube.com/results?search_query=" + text_p + "&oq=" + text_p, 
+      description: chrome.i18n.getMessage("search") + " " + matchText(text) + " " + chrome.i18n.getMessage("against") + " " + chrome.i18n.getMessage("youtube")
+    }
+  ]
+}
+
+function generalSuggest(text) {
+  text_p = text.replace(/\s+/g, "+");
+  return [
+    {
+      content: "https://www.google.com/search?q=" + text, 
+      description: chrome.i18n.getMessage("search") + " " + matchText(text) + " " + chrome.i18n.getMessage("against") + " " + chrome.i18n.getMessage("google")
+    },
+    { 
+      content: "http://en.wikipedia.org/wiki/" + text, 
+      description: chrome.i18n.getMessage("search") + " " + matchText(text) + " " + chrome.i18n.getMessage("against") + " " + chrome.i18n.getMessage("wikipedia")
+    },
+    {
+      content: "http://www.bing.com/search?q=" + text, 
+      description: chrome.i18n.getMessage("search") + " " + matchText(text) + " " + chrome.i18n.getMessage("against") + " " + chrome.i18n.getMessage("bing")
+    },
+    {
+      content: "http://www.youtube.com/results?search_query=" + text_p + "&oq=" + text_p, 
+      description: chrome.i18n.getMessage("search") + " " + matchText(text) + " " + chrome.i18n.getMessage("against") + " " + chrome.i18n.getMessage("youtube")
+    }
+  ]
+}
+
 // This event is fired each time the user updates the text in the omnibox,
 // as long as the extension's keyword mode is still active.
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
-    suggest([
-      {content: "http://slovari.yandex.ru/" + text + "/перевод/#lingvo", description: "Translate <match>" + text + "</match> with Lingvo"},
-      {content: "http://yandex.ru/yandsearch?text=" + text, description: "Искать <match>" + text + "</match> в Яндексе"},
-      {content: "http://images.yandex.ru/yandsearch?text=" + text, description: "Искать <match>" + text + "</match> в картинках Яндекса"},
-      {content: "http://ru.wikipedia.org/wiki/" + text, description: "Искать <match>" + text + "</match> в Википедии"},
-      {content: "http://en.wikipedia.org/wiki/" + text, description: "Search <match>" + text + "</match> against Wikipedia"}
-    ]);
+    if (text.match(/[\u0400-\u04FF\u0500-\u052F]+/g)) {
+      suggest(russianSuggest(text));
+    } else {
+      suggest(generalSuggest(text));
+    }
     defaultSuggestion(text);
   });
 
@@ -25,7 +75,7 @@ function navigate(url) {
 function defaultSuggestion(text) {
   chrome.omnibox.setDefaultSuggestion(text ? 
     {content: "https://www.google.com/search?q=" + text, description: "Search <match>" + text + "</match> against Google"} :
-    {description: 'Search in different engines'}
+    {description: 'Искать в <dim>Яндексе</dim>'}
   );
 }
 
@@ -39,5 +89,7 @@ chrome.omnibox.onInputEntered.addListener(
   function(text) {
     if ('http' == text.substring(0,4)) {
       navigate(text);
+    } else {
+      navigate("http://yandex.ru/yandsearch?text=" + text);
     }
   });
